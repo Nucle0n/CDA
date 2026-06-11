089544c68b4118c806544f4e654a531d26a197a8
Nucleon <thibaud.guinchard@gmail.com>
2026-06-11 13:48:51 +0200

    exo VAN

--- Algo/exercices/js/ex4_1_VAN.mjs
+++ Algo/exercices/js/ex4_1_VAN.mjs
@@ -5,7 +5,7 @@ async function main() {
     
     const sc = new createInterface({input,output});
 
-    let I0 = parseFloat(await sc.question("Investissement initial :"));
+    let init = parseFloat(await sc.question("Investissement initial :"));
     let i  = parseFloat(await sc.question("Taux d'actualisation (%) :")) / 100;
 
     let cf1 = parseFloat(await sc.question("Cash-flow année 1 :"));
@@ -13,17 +13,17 @@ async function main() {
     let cf3 = parseFloat(await sc.question("Cash-flow année 3 :"));
     let cf4 = parseFloat(await sc.question("Cash-flow année 4 :"));
 
-    let VR = parseFloat(await sc.question("Valeur résiduelle :"));
+    let vr = parseFloat(await sc.question("Valeur résiduelle :"));
 
-    let VAN =
-        -I0 +
+    let van =
+        -init +
         cf1 / Math.pow(1 + i, 1) +
         cf2 / Math.pow(1 + i, 2) +
         cf3 / Math.pow(1 + i, 3) +
         cf4 / Math.pow(1 + i, 4) +
-        VR  / Math.pow(1 + i, 4);
+        vr  / Math.pow(1 + i, 5);
         
-    console.log(`VAN = ${VAN.toFixed(2)} €`);
+    console.log(`VAN = ${van.toFixed(2)} €`);
 
     sc.close();
 }
