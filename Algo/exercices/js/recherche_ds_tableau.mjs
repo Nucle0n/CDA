3a2abd3795701995a8e5699982908f0dc3242a38
Nucleon <thibaud.guinchard@gmail.com>
2026-06-16 11:27:44 +0200

    recherche dans tableau, modif + corrigé

--- Algo/exercices/js/recherche_ds_tableau.mjs
+++ Algo/exercices/js/recherche_ds_tableau.mjs
@@ -1,29 +1,38 @@
 import {createInterface} from 'node:readline/promises';
 import {stdin as input, stdout as output} from 'node:process';
 
-function tabgenerator(tableau){
-
-    for (let i = 0; i < 10; i++) {
+// Fonction pour générer un tableau de nombres aléatoires (10 nombres)
+function tabgenerator(tableau, size){
+    
+    for (let i = 0; i < size; i++) {
 
         let rng = Math.floor(Math.random()*100)+1;
         // tab[i] = Math.floor(Math.random()*100)+1;
         
+        // On vérifie que le nombre généré n'est pas déjà présent
         if (!tableau.includes(rng)){
             tableau.push(rng);
         } else {
+            // Si le nombre généré est déjà présent on ne le place pas et on n'incrémente pas la bocule
             i--;
         }
     }
 
+    // On classe le tableau (pas utile, mais pour le fun...)
     tableau.sort((a,b) => a-b);
 
-    // console.log(tableau);
+    console.log(tableau);
 }
 async function main() {
     const sc= new createInterface({input,output});
+    let tabSize = parseInt(await sc.question('Nombre d\'éléments dans le tableau : '));
+
+    while (isNaN(tabSize) || tabSize > 100 || tabSize < 1){
+    tabSize = parseInt(await sc.question('\x1b[3;38;2;255;0;0mErreur de saisie utilisateur\x1b[0m\nEntrez un nombre entier entre 1 et 100 : '));
+    }
 
     let tab = [];
-    tabgenerator(tab);
+    tabgenerator(tab, tabSize);
     let position = 0;
 
     let answ = parseInt(await sc.question('Entrez un nombre entier positif inférieur à 101 : '));
@@ -35,7 +44,7 @@ async function main() {
     if (!tab.includes(answ)){
             console.log(`\n[404 Not found]\nLe nombre \x1b[38;2;255;255;0m${answ}\x1b[0m n'est pas présent dans le tableau\n`);
     } else {
-        for (let i = 0; i <= tab.length; i++){
+        for (let i = 0; i < tab.length; i++){
             if (tab[i] === answ){
                 position = i+1;
             }
